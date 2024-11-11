9ae164dda2d550edd11792d0b166a0fed644c86d
pkkalive <kalivepurushothamkumar@gmail.com>
2024-11-10 20:13:40 -0500

    added Header Component

---
+++ src/components/header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../../../public/assets/logo.jpg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="logo" src={logo} alt="logo" />
+      <nav className="nav-bar">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
